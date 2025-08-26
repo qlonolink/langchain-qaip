a40453a8b17e77026e807f3c102b99c51fa0a0b1
iehn <iehnrtnc@gmail.com>
2025-08-26 16:24:46 +0900

    Initial code (#1)

---
+++ langchain_qai/__init__.py
@@ -0,0 +1,7 @@
+from .retriever import QAIRetriever
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "QAIRetriever",
+]
---
+++ langchain_qai/retriever.py
@@ -0,0 +1,58 @@
+from typing import (
+    Any,
+    Dict,
+    List,
+)
+
+import requests
+from langchain_core.callbacks import CallbackManagerForRetrieverRun
+from langchain_core.documents import Document
+from langchain_core.retrievers import BaseRetriever
+from langchain_core.utils import get_from_dict_or_env
+from pydantic import model_validator
+
+QAI_SEARCH_API_URL = "https://qai.qaip.com/v1/search"
+USER_AGENT = "qai-langchain-retriever/1.0"
+
+
+class QAIRetriever(BaseRetriever):
+    api_key: str = ""
+
+    @model_validator(mode="before")
+    def validate_environment(cls, values: Dict) -> Dict:
+        values["api_key"] = get_from_dict_or_env(values, "api_key", "QAI_API_KEY")
+        return values
+
+    @property
+    def _headers(self) -> Dict[str, str]:
+        return {
+            "User-Agent": USER_AGENT,
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "x-api-key": self.api_key,
+        }
+
+    def _search(self, query: str, **keywords: Any) -> List[dict]:
+        body = {
+            "query": query,
+        }
+        for key in ["tag_ids", "source_types", "file_types", "date_from", "date_to", "domains", "limit", "offset"]:
+            if key in keywords and keywords[key]:
+                body[key] = keywords[key]
+
+        response = requests.post(url=QAI_SEARCH_API_URL, headers=self._headers, json=body, timeout=60)
+        if response.status_code != 200:
+            raise Exception(f"Error in search request: {response}")
+
+        return response.json()
+
+    def _get_relevant_documents(
+        self,
+        query: str,
+        *,
+        run_manager: CallbackManagerForRetrieverRun,
+        **keywords: Any,
+    ) -> List[Document]:
+        search_results = self._search(query, **keywords)
+
+        return [Document(page_content=result.pop("text"), metadata=result) for result in search_results["results"]]
